ce5e1f0269f59c93eac04d56617e5c1c3a21d5e4
Ritesh Jha <riteshjha0270@gmail.com>
2017-03-26 15:58:57 +0530

    message

---
+++ workspace/Text_to_Speech_Conve1/text_to_speech/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace text_to_speech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer reader;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Width = 70;
+            button1.Height = 70;
+            GraphicsPath p = new GraphicsPath();
+            p.AddEllipse(4, 4, button1.Width - 8, button1.Height - 8);
+            button1.Region = new Region(p);
+
+            reader = new SpeechSynthesizer();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            textBox1.ScrollBars = ScrollBars.Both;
+        }
+
+        //SPEAK TEXT
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reader.Dispose();
+            if (textBox1.Text != "")
+            {
+
+                reader = new SpeechSynthesizer();
+                reader.SpeakAsync(textBox1.Text);
+                label2.Text = "SPEAKING";
+                button2.Enabled = true;
+                button4.Enabled = true;
+                reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SpeakCompleted);
+            }
+            else
+            {
+                MessageBox.Show("Please enter some text in the textbox", "Message", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
+        void reader_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            label2.Text = "IDLE";
+        }
+
+        //PAUSE
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Speaking)
+                {
+                    reader.Pause();
+                    label2.Text = "PAUSED";
+                    button3.Enabled = true;
+
+                }
+            }
+        }
+
+        //RESUME
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Paused)
+                {
+                    reader.Resume();
+                    label2.Text = "SPEAKING";
+                }
+                button3.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                reader.Dispose();
+                label2.Text = "IDLE";
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            textBox1.Text =  File.ReadAllText(openFileDialog1.FileName.ToString());
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        char check;
+        private void richtext_keydown(object sender, KeyEventArgs e)
+        {if(check=='t')
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    reader.Dispose();
+                    if (textBox1.Text != "")
+                    {
+
+                        reader = new SpeechSynthesizer();
+                        reader.SpeakAsync(textBox1.Text);
+                        label2.Text = "SPEAKING";
+                        button2.Enabled = true;
+                        button4.Enabled = true;
+                        reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SpeakCompleted);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter some text in the textbox", "Message", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        private void Check_change(object sender, EventArgs e)
+        {if (checkBox1.Checked == true)
+                check = 't';
+            else
+                check = 'f';
+        }
+
+        
+    }
+}
